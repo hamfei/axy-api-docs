c7b4658032e82dd928fd82e684fb557b775c8e7b
hanfei <hanfei@email.cn>
2025-06-12 11:39:25 +0800

    测试

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,13 +3,13 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>全国公共信用大数据平台API文档</span>,
-  // project: {
-  //   link: 'https://github.com/shuding/nextra-docs-template',
-  // },
+  project: {
+    link: 'https://axinyong.com.cn',
+  },
   // chat: {
   //   link: 'https://discord.com',
   // },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: '© 2025 全民信用',
   },
